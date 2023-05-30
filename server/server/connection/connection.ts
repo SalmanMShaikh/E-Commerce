a322d81e0dc9203ec300aa4e2824a0d6ec3ae2af
jack <ss5730939@gmail.com>
2023-05-30 09:38:06 +0530

    added data

--- server/server/connection/connection.ts
+++ server/server/connection/connection.ts
@@ -6,11 +6,11 @@ dotenv.config();
 
 const AppDataSource = new DataSource({
     type: 'postgres',
-    host: process.env.DB_HOST,
-    port: parseInt(process.env.DB_PORT || '54432'),
-    username: process.env.DB_USERNAME,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_DATABASE,
+    host: process.env.DB_HOST || 'localhost',
+    port: parseInt(process.env.DB_PORT || '5432'),
+    username: process.env.DB_USERNAME || 'postgres',
+    password: process.env.DB_PASSWORD || 'a.b.c.d.',
+    database: process.env.DB_DATABASE || 'postgres',
     entities: ['server/entities/*{.ts,.js}*'],
     synchronize: true,
     logging: true
