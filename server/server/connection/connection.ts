30985817e9fbed26db1d8363b09ed3ea421d44c0
jack <ss5730939@gmail.com>
2023-05-30 09:27:22 +0530

    added enviornment variables

--- server/server/connection/connection.ts
+++ server/server/connection/connection.ts
@@ -1,13 +1,16 @@
 import { DataSource } from "typeorm"
 import { Product } from "../entities/Product"
+import dotenv from 'dotenv';
+
+dotenv.config();
 
 const AppDataSource = new DataSource({
     type: 'postgres',
-    host: 'localhost',
-    port: 5432,
-    username: 'postgres',
-    password: 'a.b.c.d.',
-    database: 'postgres',
+    host: process.env.DB_HOST,
+    port: parseInt(process.env.DB_PORT || '54432'),
+    username: process.env.DB_USERNAME,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_DATABASE,
     entities: ['server/entities/*{.ts,.js}*'],
     synchronize: true,
     logging: true
