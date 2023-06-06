0445cfb3f7e25d2e1769157dfd6202fcaf7eee83
jack <ss5730939@gmail.com>
2023-06-06 09:51:24 +0530

    removed credentials

--- server/server/connection/connection.ts
+++ server/server/connection/connection.ts
@@ -6,11 +6,11 @@ dotenv.config();
 
 const AppDataSource = new DataSource({
     type: 'postgres',
-    host: process.env.DB_HOST || 'suleiman.db.elephantsql.com',
-    port: parseInt(process.env.DB_PORT || '5432'),
-    username: process.env.DB_USERNAME || 'pkajslol',
-    password: process.env.DB_PASSWORD || 'odoKfBX2BfxkUXJtmRLShpPgEonZ_lw9',
-    database: process.env.DB_DATABASE || 'pkajslol',
+    host: process.env.DB_HOST,
+    port: parseInt(process.env.DB_PORT),
+    username: process.env.DB_USERNAME,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_DATABASE,
     entities: ['server/entities/*{.ts,.js}*'],
     synchronize: true,
     logging: true
